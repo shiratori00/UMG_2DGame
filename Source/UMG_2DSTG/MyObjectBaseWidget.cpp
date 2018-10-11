d2f43ff85be7c69987a5057be5f57ba559711bcf
shiratori1221 <shiatori0909@gmail.com>
2018-10-11 18:11:03 +0900

    オブジェクト用のベースクラスの作成

---
+++ Source/UMG_2DSTG/MyObjectBaseWidget.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyObjectBaseWidget.h"
+
+//void UMyObjectBaseWidget::Move(float x, float y)
+//{
+//	position.X += x;
+//	position.Y += y;
+//
+//	this->RenderTransform.Translation = position;
+//}
+//
+//void UMyObjectBaseWidget::SetPosition(float x, float y)
+//{
+//	position.X = x;
+//	position.Y = y;
+//
+//	this->RenderTransform.Translation = position;
+//}
---
+++ Source/UMG_2DSTG/MyObjectBaseWidget.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MyObjectBaseWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UMG_2DSTG_API UMyObjectBaseWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ObjectProperty")
+	//	FVector2D position;
+
+	//UFUNCTION(BlueprintCallable, Category = "ObjectMovement")
+	//	void Move(float x, float y);
+
+	//UFUNCTION(BlueprintCallable, Category = "ObjectMovement")
+	//	void SetPosition(float x, float y);
+};
