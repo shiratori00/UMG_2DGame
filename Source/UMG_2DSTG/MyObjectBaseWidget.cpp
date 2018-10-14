55a0154a629de22424dc3a679a8696b0e3f0ef72
shiratori0909@gmail.com <shiratori1221>
2018-10-14 16:40:52 +0900

    Titleにアニメーションとか追加

--- Source/UMG_2DSTG/MyObjectBaseWidget.cpp
+++ Source/UMG_2DSTG/MyObjectBaseWidget.cpp
@@ -2,13 +2,13 @@
 
 #include "MyObjectBaseWidget.h"
 
+//コンストラクタ？
 void UMyObjectBaseWidget::NativeConstruct()
 {
-	Super::NativeConstruct();
-
-	
+	Super::NativeConstruct();	
 }
 
+//移動処理
 void UMyObjectBaseWidget::Move(float x, float y)
 {
 	position.X += x;
@@ -17,28 +17,28 @@ void UMyObjectBaseWidget::Move(float x, float y)
 	this->SetRenderTranslation(position);
 }
 
+//位置を設定
 void UMyObjectBaseWidget::SetPosition(float x, float y)
 {
-	float offsetX = size.X / 2;
-	float offsetY = size.Y / 2;
-	//position.X = x + offsetX;
-	//position.Y = y + offsetY;
 	position.X = x;
 	position.Y = y;
 
 	this->SetRenderTranslation(position);
 }
 
+//位置を取得
 FVector2D UMyObjectBaseWidget::GetPosition()
 {
 	return position;
 }
 
+//サイズを取得
 FVector2D UMyObjectBaseWidget::GetSize()
 {
 	return size;
 }
 
+//重力処理
 void UMyObjectBaseWidget::Gravity()
 {
 	//重力を適用しない場合は処理をしない
@@ -52,6 +52,7 @@ void UMyObjectBaseWidget::Gravity()
 
 }
 
+//あたり判定
 bool UMyObjectBaseWidget::IsHitTargetActor(FVector2D targetPos, FVector2D targetSize)
 {
 	float distanceX = FMath::Abs(targetPos.X - this->position.X);
@@ -64,9 +65,11 @@ bool UMyObjectBaseWidget::IsHitTargetActor(FVector2D targetPos, FVector2D target
 	{
 		return true;
 	}
+
 	return false;
 }
 
+//画面外チェック処理
 bool UMyObjectBaseWidget::IsOutViewport(float sizeX, float sizeY)
 {
 	//trueなら範囲外
@@ -74,5 +77,6 @@ bool UMyObjectBaseWidget::IsOutViewport(float sizeX, float sizeY)
 	{
 		return true;
 	}
+
 	return false;
 }
